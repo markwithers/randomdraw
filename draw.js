ce7ca29c0ffa10db1c313f1248f59d40f34e8346
markwithers <mark26266@gmail.com>
2014-03-03 14:23:48 +0000

    Modified to allow for draws without entry counts

--- draw.js
+++ draw.js
@@ -1,3 +1,5 @@
+var WINNERS = 2;
+
 var fs = require('fs'),
   readline = require('readline'),
   path = require('path');
@@ -13,11 +15,17 @@ var rd = readline.createInterface({
 var results = []
 rd.on('line', function(line) {
   var matches = line.match(/(.*),\s*(\d*)/);
-  name = matches[1];
-  count = matches[2];
+  
+  if (matches){
+    name = matches[1];
+    count = matches[2];
 
-  for (var i = 0; i < count; i++){
-    results.push(name);
+    for (var i = 0; i < count; i++){
+      results.push(name);
+    }
+  }
+  else {
+    results.push(line);
   }
 });
 
@@ -28,7 +36,7 @@ function getRandomInt(min, max) {
 rd.on('close', function() {
   winners = {}
 
-  while(Object.keys(winners).length < 4){
+  while(Object.keys(winners).length < WINNERS){
     var pick = getRandomInt(0, results.length - 1);
     var newWinner = results[pick];
 
